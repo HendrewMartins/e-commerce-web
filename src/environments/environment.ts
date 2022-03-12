83c48bfb476cb49100f13618d80ccbd89c8c20f7
HendrewMartins <hendrewmartins@hotmail.com>
2022-03-12 11:45:34 -0300

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //api: `http://localhost:8080`
-  api: `https://fino-sabor-api.herokuapp.com`
+  api: `http://localhost:8080`
+//api: `https://fino-sabor-api.herokuapp.com`
 };
 
 /*
